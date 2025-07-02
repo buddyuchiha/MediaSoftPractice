ea4ee5af002e7ac9ce89ceb863d0a9d7b44fd50d
afrodetka <Pro100biv@mail.ru>
2025-07-02 18:43:14 +0400

    exerc 2 complete

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
 import java.util.Random;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
@@ -22,5 +27,26 @@ public class Main {
         }
         double averageAge = (double) sum / carYears.length;
         System.out.println("Средний возраст авто: " + averageAge);
+        List<String> carModels = new ArrayList<>();
+        carModels.add("Toyota Camry");
+        carModels.add("BMW X5");
+        carModels.add("Tesla Model S");
+        carModels.add("Toyota Camry");
+        carModels.add("Audi A4");
+        carModels.add("Tesla Model 3");
+
+        Set<String> uniqueModels = new TreeSet<>(Collections.reverseOrder());
+        for (String model : carModels) {
+            if (model.contains("Tesla")) {
+                uniqueModels.add("ELECTRO_CAR");
+            } else {
+                uniqueModels.add(model);
+            }
+        }
+
+        System.out.println("\nУникальные модели (обратный порядок):");
+        for (String model : uniqueModels) {
+            System.out.println(model);
+        }
     }
 }
