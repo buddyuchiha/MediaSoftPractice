ed0abfc7c41e2a1bd9ff2bfabdece06026d2f756
afrodetka <Pro100biv@mail.ru>
2025-07-02 20:03:36 +0400

    exerc 3 complete

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,23 @@
-import java.util.Random;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Set;
-import java.util.TreeSet;
-import java.util.Collections;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        int[] carYears = new int[50];
-        Random random = new Random();
+        Set<Car> carSet = new HashSet<>();
+        Car car1 = new Car("VIN123", "Camry", "Toyota", 2020, 50_000, 25_000);
+        Car car2 = new Car("VIN456", "X5", "BMW", 2021, 30_000, 60_000);
+        Car car3 = new Car("VIN123", "Corolla", "Toyota", 2019, 70_000, 20_000);
 
-        for (int i = 0; i < carYears.length; i++) {
-            carYears[i] = 2000 + random.nextInt(26);
-        }
+        System.out.println("Пытаемся добавить 3 машины (2 с одинаковым VIN):");
+        System.out.println("Добавлена car1? " + carSet.add(car1));
+        System.out.println("Добавлена car2? " + carSet.add(car2));
+        System.out.println("Добавлена car3? " + carSet.add(car3));
 
-        System.out.println("Машины после 2015 года:");
-        for (int year : carYears) {
-            if (year > 2015) {
-                System.out.println(year);
-            }
-        }
+        System.out.println("\nИтоговое количество машин в HashSet: " + carSet.size());
 
-        int sum = 0;
-        for (int year : carYears) {
-            sum += (2025 - year);
-        }
-        double averageAge = (double) sum / carYears.length;
-        System.out.println("Средний возраст авто: " + averageAge);
-        List<String> carModels = new ArrayList<>();
-        carModels.add("Toyota Camry");
-        carModels.add("BMW X5");
-        carModels.add("Tesla Model S");
-        carModels.add("Toyota Camry");
-        carModels.add("Audi A4");
-        carModels.add("Tesla Model 3");
+        List<Car> sortedCars = new ArrayList<>(carSet);
+        Collections.sort(sortedCars);
 
-        Set<String> uniqueModels = new TreeSet<>(Collections.reverseOrder());
-        for (String model : carModels) {
-            if (model.contains("Tesla")) {
-                uniqueModels.add("ELECTRO_CAR");
-            } else {
-                uniqueModels.add(model);
-            }
-        }
-
-        System.out.println("\nУникальные модели (обратный порядок):");
-        for (String model : uniqueModels) {
-            System.out.println(model);
-        }
+        System.out.println("\nМашины отсортированы по году (от новых к старым):");
+        sortedCars.forEach(car -> System.out.println(car.getYear() + " " + car.getModel()));
     }
 }
